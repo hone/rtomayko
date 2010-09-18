49e4ff8826b37d1e7c1d8996f95ee0eea177d2a1
Terence Lee <hone02@gmail.com>
2010-09-18 14:19:34 -0500

    don't pull in rspec-mocks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,11 @@ module Runner
   def run_spec(text)
     path = "#{Dir.tmpdir}/#{Process.pid}"
     File.open(path, "w") do |file|
+      file.puts <<RSPEC_CONFIGURE
+RSpec.configure do |config|
+  config.mock_with ''
+end
+RSPEC_CONFIGURE
       file.puts text
     end
     `rspec #{path} 2>&1`
@@ -12,4 +17,5 @@ end
 
 RSpec.configure do |config|
   config.include Runner
+  config.mock_with ''
 end
