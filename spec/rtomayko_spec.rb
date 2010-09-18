2c2e3b49437b457fa92f073954c7b736ba22f32f
Terence Lee <hone02@gmail.com>
2010-09-18 04:37:46 -0500

    should make second test false, so we can see if the output changed

--- spec/rtomayko_spec.rb
+++ spec/rtomayko_spec.rb
@@ -3,12 +3,13 @@ require "spec_helper"
 describe "RTomayko" do
   it "doesn't change the behavior of regular specs" do
     output = run_spec <<EOF
+      require "#{File.dirname(__FILE__) + "/../lib/rtomayko"}"
       describe "normal spec" do
         it { true.should be_false }
-        it { true.should be_true }
+        it { true.should be_false }
       end
 EOF
-    output.should =~ /2 examples, 1 failure/
+    output.should =~ /2 examples, 2 failure/
   end
 
   it "skips the execution of an example when something failed before it" do
@@ -16,7 +17,7 @@ EOF
       require "#{File.dirname(__FILE__) + "/../lib/rtomayko"}"
       describe "skip spec", :type => :rtomayko do
         it { true.should be_false }
-        it { true.should be_true }
+        it { true.should be_false }
       end
 EOF
     output.should =~ /2 examples, 1 failure, 1 skipped/
