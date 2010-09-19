fce12ca8cf828c44a60dac6e71032a632cc8f0e5
Terence Lee <hone02@gmail.com>
2010-09-19 17:23:02 -0500

    add focusd testing support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,4 +20,7 @@ end
 RSpec.configure do |config|
   config.include Runner
   config.mock_with ''
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focused => true
+  config.alias_example_to :fit, :focused => true
 end
