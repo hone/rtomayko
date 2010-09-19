2773ceaca3c4ef8675fda30c2bb591d83f0250f8
Terence Lee <hone02@gmail.com>
2010-09-19 18:00:49 -0500

    move the require 'rtomayko to rpsec

--- spec/rtomayko_spec.rb
+++ spec/rtomayko_spec.rb
@@ -1,36 +1,45 @@
 require "spec_helper"
 
 describe "RTomayko" do
+  let(:rtomayko_file) { File.dirname(__FILE__) + "/../lib/rtomayko" }
+  let(:spec) { "require '#{rtomayko_file}'\n" }
+
   it "doesn't change the behavior of regular specs" do
-    output = run_spec <<EOF
+    spec << <<EOF
       describe "normal spec" do
         it { true.should be_false }
         it { true.should be_false }
       end
 EOF
+
+    output = run_spec(spec)
+
     output.should =~ /2 examples, 2 failure/
   end
 
   context "with RTomayko" do
     it "displays the summary line properly" do
-      output = run_spec <<EOF
+      spec << <<EOF
       describe "skip spec", :type => :rtomayko do
         it { true.should be_false }
         it { true.should be_false }
       end
 EOF
+      output = run_spec(spec)
 
       output.should =~ /2 examples, 1 failure, 1 skipped/
     end
 
     it "skips the execution of an example when something failed before it" do
-      output = run_spec <<EOF
+      spec << <<EOF
       describe "skip spec", :type => :rtomayko do
         it { true.should be_false }
         it { true.should be_false }
         it { sleep(1);true.should be_false }
       end
 EOF
+
+      output = run_spec(spec)
 
       md = /Finished in (\d+(\.\d+)?) seconds/.match(output)
       execution_time = md[1].to_f
