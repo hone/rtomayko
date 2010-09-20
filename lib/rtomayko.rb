60d94f101e69b45a88519bed2dc6a437d51c4834
Terence Lee <hone02@gmail.com>
2010-09-19 21:12:59 -0500

    add dump_profile back in

--- lib/rtomayko.rb
+++ lib/rtomayko.rb
@@ -81,6 +81,8 @@ module RSpec
       class BaseTextFormatter
         def dump_summary(duration, example_count, failure_count, pending_count, skipped_count)
           super(duration, example_count, failure_count, pending_count, skipped_count)
+          # Don't print out profiled info if there are failures, it just clutters the output
+          dump_profile if profile_examples? && failure_count == 0
           output.puts "\nFinished in #{format_seconds(duration)} seconds\n"
           output.puts colorise_summary(summary_line(example_count, failure_count, pending_count, skipped_count))
         end
