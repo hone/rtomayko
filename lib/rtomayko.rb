4c9d2ddafdbe4fb844ea66039a08ab0425d551a2
Terence Lee <hone02@gmail.com>
2010-09-19 22:33:40 -0500

    add version require

--- lib/rtomayko.rb
+++ lib/rtomayko.rb
@@ -1,9 +1,10 @@
+module RTomayko
+end
+
 require File.join(File.dirname(__FILE__), "rtomayko/rspec/core/example")
 require File.join(File.dirname(__FILE__), "rtomayko/rspec/core/reporter")
 require File.join(File.dirname(__FILE__), "rtomayko/rspec/core/formatters/base_formatter")
 require File.join(File.dirname(__FILE__), "rtomayko/rspec/core/formatters/base_text_formatter")
 require File.join(File.dirname(__FILE__), "rtomayko/rspec/core/formatters/documentation_formatter")
 require File.join(File.dirname(__FILE__), "rtomayko/rspec/core/formatters/progress_formatter")
-
-module RTomayko
-end
+require File.join(File.dirname(__FILE__), "rtomayko/version")
